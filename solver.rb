ec35714cf0a572968ec1d5eeed675c4d3283a3fe
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-10 16:46:36 +0500

    Create method factorial

--- solver.rb
+++ solver.rb
@@ -1,2 +1,8 @@
 class Solver
+  def factorial(num)
+    raise Exception, 'Cant be factorial by negative numbers!' if num.negative?
+    return 1 if num <= 1
+
+    num * factorial(num - 1)
+  end
 end
