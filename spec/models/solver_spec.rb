54af06a089796a1b6b14ad6498a144da73489ad3
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-10 16:46:02 +0500

    Describe unit Test for method factorial

--- spec/models/solver_spec.rb
+++ spec/models/solver_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Solver do
   before(:each) do
     @solver = Solver.new
   end
+
+  describe 'Factorial' do
+    it 'Should pass the tests' do
+      expect(@solver.factorial(0)).to eq(1)
+      expect(@solver.factorial(4)).to eq(24)
+      expect(@solver.factorial(7)).to eq(5040)
+      expect { @solver.factorial(-1) }.to raise_error(Exception, 'Cant be factorial by negative numbers!')
+    end
+  end
 end
